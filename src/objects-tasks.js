043188902bc47e233fe7fa9b9b77578f9fed95e1
areyshka <elena.draven@mail.ru>
2024-03-10 22:40:58 +0300

    docs: add solution of removeProperties task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -51,8 +51,10 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const obj2 = obj;
+  keys.forEach((key) => delete obj2[key]);
+  return obj2;
 }
 
 /**
